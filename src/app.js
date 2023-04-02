392537b91a5d5d2160d731c4b50695fa518b9f83
Vinicius Bastos <vclemente2@hotmail.com>
2023-04-01 23:34:09 -0300

    Configuração do servidor.

--- src/app.js
+++ src/app.js
@@ -1,1 +1,19 @@
+import { config } from 'dotenv';
+import express from 'express';
+import cors from 'cors';
+config();
 
+class App {
+    constructor() {
+        this.app = express();
+
+        this.app.use(express.json());
+        this.app.use(cors());
+    }
+
+    getApp = () => {
+        return this.app;
+    }
+}
+
+export default App;
--- src/index.js
+++ src/index.js
@@ -1,1 +1,5 @@
+import App from './app.js';
 
+const app = new App().getApp();
+const port = process.env.PORT || 3000;
+app.listen(port, () => console.log(`Server is running on port ${port}`));
